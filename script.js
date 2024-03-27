989aca3d79995f62330262bfc121caab47708bc1
Sergio Vergara <sergiovergara123@gmail.com>
2024-03-27 13:10:59 -0500

    basic spreadsheet functions developed and set in spreadsheetFunctions object

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-const range = (start, end) => Array(end - start + 1).fill(start).map((element, index) => element + index);
+const isEven = (num) => num % 2 === 0 ? true : false;
+const sum = nums => nums.reduce((acc, el) => acc + el, 0);
+const average = (nums) => sum(nums) / nums.length;
+const median = (nums) => {
+  const sorted = nums.slice().sort((a, b) => a - b)
+  const length = sorted.length;
+  const middle = length / 2 - 1;
+  return isEven(length) ? average([sorted[middle], sorted[middle + 1]]) : sorted[Math.ceil(middle)];
+}
 
+const spreadsheetFunctions = {
+  sum,
+  average,
+  median,
+};
+
+const range = (start, end) =>
+  Array(end - start + 1)
+    .fill(start)
+    .map((element, index) => element + index);
+
+const charRange = (start, end) => range(start.charCodeAt(0), end.charCodeAt(0)).map((code) => String.fromCharCode(code));
 window.onload = () => {
   const container = document.getElementById("container");
   const createLabel = (name) => {
@@ -8,4 +28,16 @@ window.onload = () => {
     label.textContent = name;
     container.appendChild(label);
   };
+  const letters = charRange("A", "J");
+  letters.forEach(createLabel);
+  range(1, 99).forEach((number) => {
+    createLabel(number);
+    letters.forEach((letter) => {
+      const input = document.createElement("input");
+      input.type = "text";
+      input.id = letter + number;
+      input.ariaLabel = letter + number;
+      container.appendChild(input);
+    });
+  })
 };
